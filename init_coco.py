b16b30563696469e417802e1c8a18dec412dbed9
Yuwen Xiong <yuwen@uber.com>
2019-04-16 17:05:09 +0000

    fix a bug in coco annotation generation code

--- init_coco.py
+++ init_coco.py
@@ -6,23 +6,29 @@ if __name__ == "__main__":
 
     idx_mapping = {**{idx1: idx2 for idx1, idx2 in zip(range(53), range(80, 133))}, **{idx1: idx2 for idx1, idx2 in zip(range(53, 133), range(80))}}
     inv_idx_mapping = {**{idx1: idx2 for idx1, idx2 in zip(range(80), range(53, 133))}, **{idx1: idx2 for idx1, idx2 in zip(range(80, 133), range(53))}}
+
+    cat_json = json.load(open('lib/dataset_devkit/panopticapi/panoptic_coco_categories.json'))
+    cat_json_stff = copy.deepcopy(cat_json)
+    cat_idx_mapping = {}
+    for idx, k in enumerate(cat_json):
+        cat_idx_mapping[k['id']] = idx
+    for k, v in idx_mapping.items():      
+        cat_json_stff[k] = cat_json[v]
+        cat_json_stff[k]['id'] = k
+    json.dump(cat_json_stff, open('data/coco/annotations/panoptic_coco_categories_stff.json', 'w'))
+
     for s in ['train', 'val']:
 
         pano_json = json.load(open('data/coco/annotations/panoptic_{}2017.json'.format(s)))
 
-        cat_idx_mapping = {}
-        for idx, k in enumerate(pano_json['categories']):
-            cat_idx_mapping[k['id']] = idx
-
         pano_json_stff = copy.deepcopy(pano_json)
-        for k, v in idx_mapping.items():      
-            pano_json_stff['categories'][k] = pano_json['categories'][v]
-            pano_json_stff['categories'][k]['id'] = k
+
+        pano_json_stff['categories'] = cat_json_stff
+
         for anno in pano_json_stff['annotations']:
             for segments_info in anno['segments_info']:
                 segments_info['category_id'] = inv_idx_mapping[cat_idx_mapping[segments_info['category_id']]]
         
         json.dump(pano_json_stff, open('data/coco/annotations/panoptic_{}2017_stff.json'.format(s), 'w'))
 
-    json.dump(pano_json_stff['categories'], open('data/coco/annotations/panoptic_coco_categories_stff.json', 'w'))
 
